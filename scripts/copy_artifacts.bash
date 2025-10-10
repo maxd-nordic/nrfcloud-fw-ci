b94747c91ace7f29704912b87e8f717136048583
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2025-10-10 17:02:56 +0200

    simplify

---
+++ scripts/copy_artifacts.bash
@@ -0,0 +1,21 @@
+#!/usr/env bash
+
+# take three args, sample_name, build_dir, artifacts_dir
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 sample_name build_dir artifacts_dir"
+    exit 1
+fi
+
+SAMPLE_NAME=$1
+BUILD_DIR=$2
+ARTIFACTS_DIR=$3
+BUILD_BASENAME=$(basename $BUILD_DIR)
+TARGET_DIR=$ARTIFACTS_DIR/$BUILD_BASENAME
+
+mkdir -p $TARGET_DIR
+cp $BUILD_DIR/dfu_application.zip $TARGET_DIR
+cp $BUILD_DIR/partitions.yml $TARGET_DIR
+for file in zephyr.signed.hex .config zephyr.dts; do
+      cp $BUILD_DIR/$SAMPLE_NAME/zephyr/$file $TARGET_DIR
+      done
