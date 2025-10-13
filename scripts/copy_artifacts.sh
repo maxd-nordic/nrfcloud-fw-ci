7ead39f8e241ffc2ba428fb04c11a054fd01c50d
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2025-10-13 17:09:10 +0200

    fix binary log build

--- scripts/copy_artifacts.sh
+++ scripts/copy_artifacts.sh
@@ -14,13 +14,16 @@ BUILD_BASENAME=$(basename $BUILD_DIR)
 TARGET_DIR=$ARTIFACTS_DIR/$BUILD_BASENAME
 
 mkdir -p $TARGET_DIR
-cp -f $BUILD_DIR/dfu_application.zip $TARGET_DIR
-cp -f $BUILD_DIR/dfu_mcuboot.zip $TARGET_DIR
-cp -f $BUILD_DIR/partitions.yml $TARGET_DIR
-cp -f $BUILD_DIR/build_info.yml $TARGET_DIR
-cp -f $BUILD_DIR/merged*.hex $TARGET_DIR
-cp -f $BUILD_DIR/$SAMPLE_NAME/zephyr/.config $TARGET_DIR/dotconfig.txt
-cp -f $BUILD_DIR/$SAMPLE_NAME/zephyr/zephyr.signed.hex $TARGET_DIR
-cp -f $BUILD_DIR/$SAMPLE_NAME/zephyr/zephyr.dts $TARGET_DIR
+cp $BUILD_DIR/dfu_application.zip $TARGET_DIR
+cp $BUILD_DIR/dfu_mcuboot.zip $TARGET_DIR
+cp $BUILD_DIR/partitions.yml $TARGET_DIR
+cp $BUILD_DIR/build_info.yml $TARGET_DIR
+cp $BUILD_DIR/merged*.hex $TARGET_DIR
+cp $BUILD_DIR/zephyr/.config $TARGET_DIR/zephyr-dotconfig.txt
+cp $BUILD_DIR/$SAMPLE_NAME/zephyr/.config $TARGET_DIR/app-dotconfig.txt
+cp $BUILD_DIR/$SAMPLE_NAME/zephyr/.config.sysbuild $TARGET_DIR/app-dotconfig.sysbuild.txt
+cp $BUILD_DIR/$SAMPLE_NAME/zephyr/zephyr.signed.hex $TARGET_DIR
+cp $BUILD_DIR/$SAMPLE_NAME/zephyr/zephyr.dts $TARGET_DIR
+cp $BUILD_DIR/$SAMPLE_NAME/zephyr/log_dictionary.json $TARGET_DIR
 
 exit 0
