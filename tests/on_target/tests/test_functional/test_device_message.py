f1da9087b211c10f58d5700c717bea92c97519f8
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2025-10-27 17:04:35 +0100

    check message

--- tests/on_target/tests/test_functional/test_device_message.py
+++ tests/on_target/tests/test_functional/test_device_message.py
@@ -8,6 +8,8 @@ from utils.logger import get_logger
 
 logger = get_logger()
 
+CLOUD_TIMEOUT = 60 * 3
+
 def test_device_message(dut_cloud, coap_device_message_hex_file):
     '''
     Test that verifies that device can connect to nRF Cloud and send device messages.
@@ -25,3 +27,22 @@ def test_device_message(dut_cloud, coap_device_message_hex_file):
         ],
         timeout=240
     )
+
+    # Wait for message to be reported to cloud
+    start = time.time()
+    while time.time() - start < CLOUD_TIMEOUT:
+        time.sleep(5)
+        messages = dut_cloud.cloud.get_messages(dut_cloud.device_id, appname=None, max_records=20, max_age_hrs=0.25)
+        logger.debug(f"Found messages: {messages}")
+
+        latest_message = messages[0] if messages else None
+        logger.info(f"Latest message: {latest_message}")
+        if latest_message:
+            check_message_age = dut_cloud.cloud.check_message_age(message=latest_message, seconds=30)
+            if check_message_age:
+                break
+        else:
+            logger.debug("No message with recent timestamp, retrying...")
+            continue
+    else:
+        raise RuntimeError("No new message to cloud observed")
