9506b95e3249f83c26abbcabafe0d81fab2df81d
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2025-11-04 16:08:48 +0100

    rest device message support

--- tests/on_target/tests/test_functional/test_device_message.py
+++ tests/on_target/tests/test_functional/test_device_message.py
@@ -10,10 +10,14 @@ logger = get_logger()
 
 CLOUD_TIMEOUT = 60 * 3
 
-def test_device_message(dut_cloud, coap_device_message_hex_file):
+def test_coap_device_message(dut_cloud, coap_device_message_hex_file):
     '''
-    Test that verifies that device can connect to nRF Cloud and send device messages.
+    Test that verifies that device can connect to nRF Cloud CoAP and send device messages.
     '''
+
+    if SAMPLE_GROUP != "coap":
+        pytest.skip("Skipping CoAP FOTA test for non-CoAP sample group")
+
     flash_device(os.path.abspath(coap_device_message_hex_file))
     dut_cloud.uart.xfactoryreset()
     dut_cloud.uart.flush()
@@ -42,6 +46,48 @@ def test_device_message(dut_cloud, coap_device_message_hex_file):
             message_content = message_object.get('sample_message', '')
 
             if "Hello World, from the CoAP Device Message Sample!" in message_content:
+                break
+        else:
+            logger.debug("No message with recent timestamp, retrying...")
+            continue
+    else:
+        raise RuntimeError("No new message to cloud observed")
+
+def test_rest_device_message(dut_cloud, rest_device_message_hex_file):
+    '''
+    Test that verifies that device can connect to nRF Cloud REST and send device messages.
+    '''
+
+    if SAMPLE_GROUP != "rest":
+        pytest.skip("Skipping REST FOTA test for non-REST sample group")
+
+    flash_device(os.path.abspath(coap_device_message_hex_file))
+    dut_cloud.uart.xfactoryreset()
+    dut_cloud.uart.flush()
+
+    test_start_time = time.time()
+    reset_device()
+
+    dut_cloud.uart.wait_for_str_ordered(
+        [
+            "Connected to LTE",
+            "Sent Hello World message with ID"
+        ],
+        timeout=CLOUD_TIMEOUT
+    )
+
+    # Poll for message to be reported to cloud
+    start = time.time()
+    while time.time() - start < CLOUD_TIMEOUT:
+        time.sleep(5)
+        messages = dut_cloud.cloud.get_messages(dut_cloud.device_id, appname=None, max_records=20, start=test_start_time)
+        logger.debug(f"Found messages: {messages}")
+
+        if messages:
+            message_object = messages[0][1]
+            message_content = message_object.get('sample_message', '')
+
+            if "Hello World, from the REST Device Message Sample!" in message_content:
                 break
         else:
             logger.debug("No message with recent timestamp, retrying...")
